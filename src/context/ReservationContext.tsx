15a60e9a41b7cf2fd78616a726e42dbbaa694521
Fanny Saez <saez.fanny.63@gmail.com>
2026-09-23 16:43:03 +0200

    feat(context): define Reservation type and context for MongoDB

--- src/context/ReservationContext.tsx
+++ src/context/ReservationContext.tsx
@@ -1,20 +1,20 @@
 import { createContext } from "react";
 
 export type Reservation = {
-    id:string,
-    salle_id:string,
-    date_debut:string,
-    date_fin:string,
-    user_id:string
+    _id: string,
+    userId: string,
+    roomId: string,
+    startDate: string,
+    endDate: string,
 }
 
 export interface ReservationContextType extends Reservation {
-    getReservation:(id:string)=>void;
-    postReservation:(reservation:Reservation)=>void;
-    putReservation:(id:string, reservation:Reservation)=>void;
-    deleteReservation:(id:string)=>void;
-    getReservationList:()=>void;
-    reservationList:Reservation[];
+    getReservation: (_id: string) => void;
+    postReservation: (reservation: Reservation) => void;
+    putReservation: (_id: string, reservation: Reservation) => void;
+    deleteReservation: (_id: string) => void;
+    getReservationList: () => void;
+    reservationList: Reservation[];
 }
 
 export const ReservationContext = createContext<ReservationContextType>(
