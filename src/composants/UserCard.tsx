84b26f237d6b603ecb553fdd85591840ae85240a
Fanny Saez <saez.fanny.63@gmail.com>
2026-09-23 16:45:07 +0200

    fix(composant): replace id with _id in User interface

--- src/composants/UserCard.tsx
+++ src/composants/UserCard.tsx
@@ -1,9 +1,9 @@
 export interface User {
-    id: string,
+    _id: string,
     firstname: string,
     lastname: string,
     email: string,
-    roleId: string
+    roleId: string,
     roleLabel: string;
 }
 
@@ -14,15 +14,12 @@ interface UserCardProps {
 function UserCard({ user }: UserCardProps) {
     return (
         <>
-
             <div className="w-full max-w-sm rounded-lg border border-gray-200 bg-white p-4 shadow-sm">
                 <p className="text-sm text-gray-500">Prénom : <span className="font-medium text-gray-900">{user.firstname}</span></p>
                 <p className="text-sm text-gray-500">Nom : <span className="font-medium text-gray-900">{user.lastname}</span></p>
                 <p className="text-sm text-gray-500">Email : <span className="font-medium text-gray-900">{user.email}</span></p>
             </div>
-
         </>
-
     );
 }
 
